f6256d3719022b833ea79611dda689598306ca86
Pravinlkr <pravinlakhisarai@gmail.com>
2021-07-26 16:07:23 +0530

    modified maincomponent.vue, removed unordered list and added a table to show todo tasks, added two button edit and delete and added functionality to edit or delete an existing task

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -4,9 +4,20 @@
     <input v-on:keyup.enter="addValue()" type="text" v-model="task">
     <br/><br/>
     <p>To Do Tasks</p>
-    <ul>
-        <li v-for="(t,index) in toDoList" v-bind:key="index">{{t}}</li>
-    </ul>
+    <table>
+        <tr style="color:white; background-color:black">
+            <th>Index</th>
+            <th>Task Name</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+        <tr v-for="(t,index) in toDoList" v-bind:key="index">
+            <td><b>{{index+1}}</b></td>
+            <td><b>{{t}}</b></td>
+            <td><button class="editButton" v-on:click="editTask(index)">Edit</button></td>
+            <td><button class="deleteButton" v-on:click="deleteTask(index)">Delete</button></td>
+        </tr>
+    </table>
 </template>
 <script>
 export default{
@@ -14,15 +25,34 @@ export default{
     data(){
         return{
             task:'',
+            editTaskIndex:null,
+            isEdit:false,
             toDoList:[]
         }
     },
     methods:{
         addValue(){
             if(this.task.length > 0){
-                this.toDoList.push(this.task);
-                this.task = '';
+                if(this.isEdit){
+                    this.toDoList[this.editTaskIndex] = this.task;
+                    this.task = '';
+                    this.isEdit = false;
+                    this.editTaskIndex = null;
+
+                }
+                else{
+                    this.toDoList.push(this.task);
+                    this.task = '';
+                }
             }   
+        },
+        deleteTask(index){
+            this.toDoList.splice(index,1);
+        },
+        editTask(index){
+            this.task = this.toDoList[index];
+            this.editTaskIndex = index;
+            this.isEdit = true;
         }
     }
 }
@@ -36,5 +66,25 @@ input{
     width:20%;
     border:1px solid #d9d9d9;
     outline:none;
+}
+table{
+    width:80%;
+    margin:2% auto;
+}
+.editButton{
+    background-color:#54a3e3;
+    color:white;
+    border:none;
+    outline:none;
+    height:30px;
+    width:30%;
+}
+.deleteButton{
+    background-color:#de2821;
+    color:white;
+    border:none;
+    outline:none;
+    height:30px;
+    width:30%;
 }
 </style>
